fc6e515aae34dfa6f782c32ee45e8743b3579d4a
Tibor Répási <rtib@users.noreply.github.com>
2017-11-23 19:16:34 +0100

    private_key_file to be String
    closes #2

--- types/secret/privkey.pp
+++ types/secret/privkey.pp
@@ -1,6 +1,6 @@
 type Ipsec::Secret::Privkey = Struct[{
   type             => Enum['RSA','ECDSA','P12'],
-  private_key_file => Stdlib::Absolutepath,
+  private_key_file => String,
   prompt           => Optional[Boolean],
   passphrase       => Optional[String],
 }]
